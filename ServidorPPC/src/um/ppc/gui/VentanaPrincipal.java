67a63e8b2f392dbca917ae61528c48a5440feceb
leandro <leandroguillen@gmail.com>
2012-10-10 18:14:47 +0200

    Diseño inicial de la GUI. Ventana principal con los botones para controlar el servidor.

--- ServidorPPC/src/um/ppc/gui/VentanaPrincipal.java
+++ ServidorPPC/src/um/ppc/gui/VentanaPrincipal.java
@@ -3,10 +3,21 @@ package um.ppc.gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.DefaultComboBoxModel;
 
 public class VentanaPrincipal {
 
-	private JFrame frame;
+	private JFrame frmServidorPpc;
 
 	/**
 	 * Launch the application.
@@ -16,7 +27,7 @@ public class VentanaPrincipal {
 			public void run() {
 				try {
 					VentanaPrincipal window = new VentanaPrincipal();
-					window.frame.setVisible(true);
+					window.frmServidorPpc.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +46,80 @@ public class VentanaPrincipal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmServidorPpc = new JFrame();
+		frmServidorPpc.setTitle("Servidor PPC");
+		frmServidorPpc.setResizable(false);
+		frmServidorPpc.setBounds(100, 100, 406, 354);
+		frmServidorPpc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmServidorPpc.getContentPane().setLayout(new BoxLayout(frmServidorPpc.getContentPane(), BoxLayout.X_AXIS));
+		
+		JPanel panel = new JPanel();
+		frmServidorPpc.getContentPane().add(panel);
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("52px:grow"),
+				ColumnSpec.decode("52px"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("49px"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("41px"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("41px"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("44px"),
+				FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
+				ColumnSpec.decode("1px"),},
+			new RowSpec[] {
+				FormFactory.LINE_GAP_ROWSPEC,
+				RowSpec.decode("30px"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JButton btnIniciar = new JButton("Iniciar");
+		panel.add(btnIniciar, "3, 2, 2, 1, fill, center");
+		
+		JButton btnParar = new JButton("Parar");
+		panel.add(btnParar, "6, 2, 3, 1, fill, center");
+		
+		JButton btnCerrar = new JButton("Salir");
+		panel.add(btnCerrar, "12, 2, 3, 1, fill, top");
+		
+		JLabel lblModo = new JLabel("Modo:");
+		panel.add(lblModo, "3, 4, left, center");
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"ASN1", "XML", "JSON"}));
+		panel.add(comboBox, "4, 4, 5, 1, fill, center");
+		
+		JLabel lblSalida = new JLabel("Salida:");
+		panel.add(lblSalida, "3, 6, left, center");
+		
+		JTextArea textArea = new JTextArea();
+		panel.add(textArea, "3, 8, 12, 17, fill, fill");
 	}
-
 }
